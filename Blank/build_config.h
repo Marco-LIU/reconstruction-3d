544e6cfe940537e72d10185b8bf12c83c0b0f7b8
MarcoBright@gmail.com <MarcoBright@gmail.com@7490ff5d-073b-4550-533e-cd5a0458d319>
2014-12-07 01:53:22 +0000

    add readme

--- Blank/build_config.h
+++ Blank/build_config.h
@@ -3,4 +3,8 @@
 // 是否启用pumping的profile，用来测试性能
 // 日志输出到debug.log中
 // 推荐在release模式使用
+#ifdef DEBUG
+#define ENABLE_CAMERA_PUMP_PROFILE 0
+#else
 #define ENABLE_CAMERA_PUMP_PROFILE 1
+#endif // DEBUG
---
+++ README.txt
@@ -0,0 +1,5 @@
+仅支持VS2010，推荐VS2010SP1
+
+编译前：
+	1、执行blank/run.bat
+	2、把dependencies/WinSDK80.rar解压缩到当前目录
